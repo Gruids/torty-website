41d4f1b96158dec7432274ee09d9af3ca1a9d848
Gruids <andrn218@gmail.com>
2026-04-29 22:19:30 +0300

    Update [key].js

--- api/categories/[key].js
+++ api/categories/[key].js
@@ -5,6 +5,7 @@ module.exports = async (req, res) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'DELETE, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+  
   if (req.method === 'OPTIONS') return res.status(200).end();
 
   if (req.method === 'DELETE') {
@@ -21,5 +22,6 @@ module.exports = async (req, res) => {
       return res.status(500).json({ error: error.message });
     }
   }
+  
   return res.status(405).json({ error: 'Method not allowed' });
 };
